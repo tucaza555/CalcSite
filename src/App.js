c1b1fc499a16a14747f8c6ff6a308dfe0ba3d3ea
ZacTucak-103056938 <tucaza555@gmail.com>
2022-05-24 23:39:51 +1000

    Setup files
    removed unneeded files in the template and set up for development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Universal Calculator App</h1>
+      <div className='calcContainer'>
+        <input></input>
+        <select name="cars" id="cars">
+          <option value="plus">+</option>
+          <option value="minus">-</option>
+          <option value="times">x</option>
+          <option value="divide">/</option>
+        </select>
+        <input></input>
+        <button>=</button>
+        <p>answer</p>
+      </div>
     </div>
   );
 }
